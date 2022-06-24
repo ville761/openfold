cd4153d076d7b2a164ff1b832b3802167353f677
Ville Uski <ville761@gmail.com>
2022-06-24 19:13:49 +0000

    fix sequence handling in run_openfold.sh

--- run_openfold.sh
+++ run_openfold.sh
@@ -5,7 +5,7 @@
 shopt -s extglob
 
 function usage {
-	echo "Usage: $0 [-p prefix] [-m {1-5}] [-t mtype] [-d device] [-r] input.fasta"
+	echo "Usage: $0 [-o output] [-m {1-5}] [-t mtype] [-d device] [-r] input.fasta"
 	echo "where the option -m sets the AlphaFold model to be used (default: 1). It can be a range, such as 1-5."
     echo "There are 5 models, so the model numbers should in the range 1-5."
     echo ""
@@ -39,8 +39,6 @@ function needs_arg() {
 # default options
 model=1 # use the first model by default
 mtype="monomer" # default AlphaFold model type
-defaultprefix="prefix"
-prefix=$defaultprefix
 modelrange="1-5"
 save_outputs="false"
 device="cpu"
@@ -52,7 +50,8 @@ while getopts "hrd:p:m:t:-:" OPT; do
         OPTARG="${OPTARG#=}"       
     fi
     case "$OPT" in
-        p | prefix) needs_arg; prefix="$OPTARG" ;;
+        o | output) needs_arg; output="$OPTARG" ;;
+        p | prefix) needs_arg; output="$OPTARG" ;; # for compatibility with an older version
         m | model) needs_arg; model="$OPTARG" ;;
         t | model_type) needs_arg; mtype="$OPTARG" ;;
         d | device) needs_arg; device="$OPTARG" ;;
@@ -63,6 +62,11 @@ done
 shift $((OPTIND-1))
 if [ $# -gt 0 ]; then
     fasta=$1 # if the file name is not given, the sequence is read from stdin
+fi
+
+if [ -z "$output" ]; then
+    echo "no output directory given"
+    exit_abnormal
 fi
 
 # model type should be either "pTM" or "monomer" (case insensitive)
@@ -81,14 +85,14 @@ else
     exit_abnormal
 fi
 
-# check if the prefix includes a path (if given on the command line)
+# check if the output directory includes a path (if given on the command line)
 # if it doesn't include an absolute path, put the output directory in the current directory
-if [[ "$prefix" =~ .*"/".* ]]; then
-	  outputdir=$prefix
-	  prefix=$( basename $prefix )
+if [[ "$output" =~ .*"/".* ]]; then
+	  outputdir=$output
+	  output=$( basename $output )
 	  [[ $outputdir != /* ]] && outputdir=$PWD/$outputdir
 else
-	  outputdir=$PWD/$prefix
+	  outputdir=$PWD/$output
 fi
 
 if [ ! -d ${outputdir} ] && [ ! -h ${outputdir} ]
@@ -103,7 +107,7 @@ then
 fi
 
 # check that the sequence if given either in stdin or as a file
-if [ "$fasta" = "" ] && [ -t 0 ] 
+if [ -z "$fasta" ] && [ -t 0 ] 
 then
 	echo "no sequence given" 
 	exit_abnormal
@@ -118,25 +122,26 @@ tmpfasta=${fastadir}/tmp.fasta
 mkdir -p ${fastadir}
 rm ${tmpfasta} 2> /dev/null
 touch ${tmpfasta}
-while IFS= read -r line
+while IFS= read -r line || [ -n "$line" ]
 do 
-	if [[ $line = \>* ]]
-	then
-		if [ "$prefix" = "$defaultprefix" ]; then
-			prefix="${line#*>}"
-			prefix="${prefix%%[^a-zA-Z0-9_]*}"
-		fi
-	else
-		echo -n $line >> ${tmpfasta}
-	fi
+    if [[ $line = \>* ]]
+    then
+        # take the characters on the first line until the first space (OpenFold will otherwise return an error)
+        line=$(cut -d ' ' -f 1 <<< "$line")
+        line=$(echo $line | tr -cd '[:alnum:]>_')
+	    echo $line >> ${tmpfasta}
+        prefix=${line#*>}
+    else
+	    echo -n $line >> ${tmpfasta}
+    fi
 done < "${fasta:-/dev/stdin}"
+#done < /dev/stdin
 echo "" >> ${tmpfasta}
 
 # append the prefix to the fasta file
-echo -e ">${prefix}\n$(cat ${tmpfasta})" > ${tmpfasta}
+#echo -e ">${prefix}\n$(cat ${tmpfasta})" > ${tmpfasta}
 
-# remove special characters
-sed -i $'s/[^[:print:]\t]//g' ${tmpfasta}
+fasta=${tmpfasta}
 
 scriptdir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -144,7 +149,7 @@ cd ${scriptdir}
 source scripts/activate_conda_env.sh
 
 echo "Precomputing protein alignments..."
-python my_precompute_alignments_mmseqs.py ${tmpfasta}  \
+python my_precompute_alignments_mmseqs.py ${fasta}  \
 	    uniref30_2103_db \
 	    ${aligndir}/ \
     --hhsearch_binary_path /usr/bin/hhsearch \
@@ -171,6 +176,7 @@ do
     else
         model="$modelnumber"
     fi
+    #cmd="python run_pretrained_openfold_all_ptm_with_plots.py ${tmpfasta} /data/db/pdb_mmcif/mmcif_files/"
     cmd="python run_pretrained_openfold.py ${tmpfasta} /data/db/pdb_mmcif/mmcif_files/"
     cmd="$cmd --use_precomputed_alignments ${aligndir}/"
     cmd="$cmd --output_dir ${outputdir}"
@@ -187,7 +193,3 @@ do
 done
 
 source scripts/deactivate_conda_env.sh
-
-# zip the output
-cd $outputdir/..
-tar zcvf $prefix.tar.gz $prefix
